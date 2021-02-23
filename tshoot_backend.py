487d6dbd8f51523f154f7c4d51eae8018f8a7150
Shahzad Qadir <shahzad@shahzadqadir.net>
2021-02-23 13:34:39 +0000

    added reading of commands from file input

--- tshoot_backend.py
+++ tshoot_backend.py
@@ -3,44 +3,20 @@
 from cisco import show_cmd_ssh
 # show_cmd_ssh(hostname, username, password, command): 
 
-class TshootBackend:
-    def __init__(self, username, password):
-        self.username = username
-        self.password = password
-    
-    def get_config(self, list_of_commands, list_of_devices):
-        device_output = {}
-  
-        for device in list_of_devices:
-            outputs = []
-            for command in list_of_commands:
-                if not show_cmd_ssh(device, self.username, self.password, command):
-                    return False
-                for data in show_cmd_ssh(device, self.username, self.password, command):
-                    outputs.append(data.strip('\r\n'))
-            device_output[device] = outputs
-        return device_output
-
-# user_name = input("username: ")
-# password = input("password: ")
-# print(user_name, password)
-# list_of_commands = input("List of Commands (python list): ").split(",")
-# list_of_devices = input("List of devices (python list): ").split(",")
-# tshoot = TshootBackend(user_name, password)
-
-
 device_ip = input("device name? ")
+user_name = input("username: ")
 device_password = input("device password: ")
-list_of_commands = [
-    'term length 0',
-    'show mpls ldp discovery',
-    'show mpls ldp neighbor detail',
-    'show run | inc session protection',
-    'show mpls ldp nei | inc Target'
-]
+filename = input("Filename for commands to run: ")
+
+list_of_commands = []
+with open(filename) as read_file:
+    for line in read_file:
+        list_of_commands.append(line.strip('\n'))
+print(list_of_commands)
+
 with open(f"{device_ip}", "w") as file:
     for command in list_of_commands:
-        result = show_cmd_ssh(device_ip, "qadirs", device_password, command)
+        result = show_cmd_ssh(device_ip, user_name, device_password, command)
         file.write('\n' + command + "\n")
         for data in result:            
             file.write(data.strip('\r\n'))
